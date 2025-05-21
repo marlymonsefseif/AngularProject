40fbe8776ab8cd0d1e9348ee2c34f623d0b54935
hageryasser2002 <hagerelkaood@gmail.com>
2025-05-21 16:22:28 +0300

    'home,headerandfooterpage'

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { UserService } from '../../services/user.service';
+import { DynamicServiceService } from '../../services/dynamic-service.service';
+import { ThisReceiver } from '@angular/compiler';
 
 @Component({
   selector: 'app-home',
@@ -11,8 +13,20 @@ import { UserService } from '../../services/user.service';
 export class HomeComponent {
   userId:any = localStorage.getItem("UserId");
   token : any = localStorage.getItem("UserAuthToken");
+  spaces!:any;
+  memberShips!:any;
+  aminities!:any;
 
-  constructor(private userService:UserService, private router:Router){}
+  constructor(private userService:UserService, private router:Router,
+              private spaceService:DynamicServiceService){}
+
+  
+  ngOnInit(): void {
+  this.spaces= this.spaceService.getAllSpaces();
+  this.memberShips=this.spaceService.getAllMemberships();
+  this.aminities=this.spaceService.getAllAminities();
+  }
+
 
   getuser() {
     if (this.token) {
